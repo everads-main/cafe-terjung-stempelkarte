20e2130990ce710786291639a6fc71efcee276fd
everads-main <theproductvideo@gmail.com>
2026-08-24 19:39:03 +0200

    Show the real Baeckerei Terjung wordmark

--- src/components/terjung-logo.tsx
+++ src/components/terjung-logo.tsx
@@ -6,11 +6,12 @@ export function TerjungLogo({
 }) {
   return (
     <div className={`overflow-hidden rounded-2xl bg-white p-3 shadow-sm ${className}`}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/terjung-logo.svg"
         alt="Bäckerei Terjung"
-        width={640}
-        height={427}
+        width={560}
+        height={254}
         className="h-auto w-full"
       />
     </div>
